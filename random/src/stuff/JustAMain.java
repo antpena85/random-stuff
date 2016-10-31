5995e935301d747742127a12751f7ce1e7258e5d
Antonio Pena <antpena85@gmail.com>
2016-10-31 12:18:00 -0400

    Added just a main class

---
+++ random/src/stuff/JustAMain.java
@@ -0,0 +1,9 @@
+package stuff;
+
+public class JustAMain {
+
+	public static void main(String[] args) {
+		System.err.println("This is just a main and location where I will initialize other classes");
+	}
+
+}
