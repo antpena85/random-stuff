12c9c6bfef172a92c7cfecfb3c2a1893339d63b1
Antonio Pena <antpena85@gmail.com>
2016-11-02 22:13:47 -0400

    r

--- random/src/stuff/JustAMain.java
+++ random/src/stuff/JustAMain.java
@@ -4,6 +4,13 @@ public class JustAMain {
 
 	public static void main(String[] args) {
 		System.err.println("This is just a main and location where I will initialize other classes");
+		
+		String x = "Hi";
+		
+		String s = new String(x);
+		
+		System.out.println(x==s); //should return false
+		System.out.println(x.equals(s));//should return true
 	}
 
 }
