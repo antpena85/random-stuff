ecd6f3977c8a39b7f7cf2b90a81cb827b2a6e9f2
Antonio Pena <antpena85@gmail.com>
2016-11-11 19:03:00 -0500

    keeping up to date

--- random/src/stuff/JustAMain.java
+++ random/src/stuff/JustAMain.java
@@ -1,9 +1,12 @@
 package stuff;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JustAMain {
 
 	public static void main(String[] args) {
-		System.err.println("This is just a main and location where I will initialize other classes");
+		System.out.println("This is just a main and location where I will initialize other classes");
 		
 		String x = "Hi";
 		
@@ -11,6 +14,16 @@ public class JustAMain {
 		
 		System.out.println(x==s); //should return false
 		System.out.println(x.equals(s));//should return true
+		
+		System.out.println("------------------------------------------------------------------------------------");
+		
+		String[] stringArray = {"I","have","two","daughters","named","Annali","and","Lianna"};
+		
+		//List<String> arrayToList = (List<String>) Arrays.;
+		
+		for(String z: arrayToList){
+			System.out.print(" " +s);
+		}
 	}
 
 }
